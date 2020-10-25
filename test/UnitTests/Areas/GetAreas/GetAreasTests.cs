7ae76fdb6e4bae8248091a99268f57cb23ff4677
criegt <criegt@outlook.com>
2020-10-25 16:27:57 -0500

    Crea proyecto para pruebas unitarias.

---
+++ test/UnitTests/Areas/GetAreas/GetAreasTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using OneLinkTest.Application.UseCases.Areas.GetAreas;
+using OneLinkTest.Domain.Areas;
+using OneLinkTest.Infrastructure.DataAccess;
+using OneLinkTest.Infrastructure.DataAccess.Repositories;
+
+namespace UnitTests.Areas.GetAreas
+{
+    public class GetAreasTests
+    {
+        private IAreaRepository _repository;
+        private IGetAreasUseCase _useCase;
+
+        [SetUp]
+        public void Setup()
+        {
+            _repository = new AreaRepositoryFake(new OneLinkTestContextFake());
+            _useCase = new GetAreasUseCase(_repository);
+        }
+
+        [Test]
+        public async Task Should_ReturnNotNull()
+        {
+            var presenter = new GetAreasPresenter();
+
+            _useCase.SetOutputPort(presenter);
+
+            await _useCase.Execute();
+
+            Assert.IsNotNull(presenter.Areas);
+        }
+    }
+}
